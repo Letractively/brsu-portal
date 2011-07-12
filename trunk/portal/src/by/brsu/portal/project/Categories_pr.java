a37d8df5dc18d959214eb09e51461de7a3ede0a8
boie8888@gmail.com <boie8888@gmail.com@ea57eaa0-7ecd-95af-890e-8a5d75c84b2e>
2011-07-12 20:13:14 +0000

    The class basis (Categories_pr) is created. Equals, hashCode, toString, getters, setters is created  only!!!

---
+++ trunk/portal/src/by/brsu/portal/project/Categories_pr.java
@@ -0,0 +1,104 @@
+/**
+ * BrSU Projects Portal
+ * (c) 2011, BrSU Java Group
+ */
+package by.brsu.portal.project;
+
+/**
+ * @author Roman Ulezlo
+ * 
+ */
+public class Categories_pr {
+	private long id_category;
+	private String name;
+
+	/**
+	 * @param id_category
+	 * @param name
+	 */
+	public Categories_pr(long id_category, String name) {
+		super();
+		this.id_category = id_category;
+		this.name = name;
+	}
+
+	/**
+	 * @return the id_category
+	 */
+	public long getId_category() {
+		return id_category;
+	}
+
+	/**
+	 * @param id_category
+	 *            the id_category to set
+	 */
+	public void setId_category(long id_category) {
+		this.id_category = id_category;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id_category ^ (id_category >>> 32));
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categories_pr other = (Categories_pr) obj;
+		if (id_category != other.id_category)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Categories_pr [id_category=" + id_category + ", name=" + name
+				+ "]";
+	}
+
+}
