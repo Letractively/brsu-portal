b7814729c8187e71253cc1701f0f3af2b1509d3f
boie8888@gmail.com <boie8888@gmail.com@ea57eaa0-7ecd-95af-890e-8a5d75c84b2e>
2011-07-12 19:57:23 +0000

    The class basis (Technologies) is created. Equals, hashCode, toString, getters, setters is created  only!!!

---
+++ trunk/portal/src/by/brsu/portal/cv/Technologies.java
@@ -0,0 +1,93 @@
+/**
+ * BrSU Projects Portal
+ * (c) 2011, BrSU Java Group
+ */
+package by.brsu.portal.cv;
+
+/**
+ * @author Roman Ulezlo
+ * 
+ */
+public class Technologies {
+	private long id_tech;
+	private String name;
+
+	/**
+	 * @return the id_tech
+	 */
+	public long getId_tech() {
+		return id_tech;
+	}
+
+	/**
+	 * @param id_tech
+	 *            the id_tech to set
+	 */
+	public void setId_tech(long id_tech) {
+		this.id_tech = id_tech;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id_tech ^ (id_tech >>> 32));
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Technologies other = (Technologies) obj;
+		if (id_tech != other.id_tech)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Technologies [id_tech=" + id_tech + ", name=" + name + "]";
+	}
+
+}
