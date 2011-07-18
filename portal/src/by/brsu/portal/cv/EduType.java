c01c8d2f8a1663b6d5ab5df8475a9a6600c0ba35
boie8888 <boie8888@gmail.com>
2011-07-18 15:53:57 +0000

    Add enum EduType. Delete class Type_edu.

---
+++ portal/src/by/brsu/portal/cv/EduType.java
@@ -0,0 +1,13 @@
+/**
+ * BrSU Projects Portal
+ * (c) 2011, BrSU Java Group
+ */
+package by.brsu.portal.cv;
+
+/**
+ * @author Roman Ulezlo
+ * 
+ */
+public enum EduType {
+	SCHOOL, COLLEDGE, UNIVERSITY, INSTITUTE;
+}
