aba24176d0b189ffc7abe6420c39b0af3278bb02
boie8888@gmail.com <boie8888@gmail.com@ea57eaa0-7ecd-95af-890e-8a5d75c84b2e>
2011-07-26 19:59:50 +0000

    Create test.

--- trunk/portal/test/by/brsu/portal/cv/TechnologyTest.java
+++ trunk/portal/test/by/brsu/portal/cv/TechnologyTest.java
@@ -4,10 +4,35 @@
  */
 package by.brsu.portal.cv;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import by.brsu.portal.cv.TechnologyDAO;
+
 /**
- * @author Admin
- *
+ * @author Roman Ulezlo
+ * 
  */
 public class TechnologyTest {
-
+	@Test
+	public void testCreateTechnology() {
+		try {
+			TechnologyDAO td = new TechnologyDAO();
+			Technology t = new Technology();
+			t = td.createTechnology("developer");
+			System.out.println(t);
+		} catch (Exception ex) {
+			Assert.fail();
+		}
+	}
+	@Test
+	public void testDeleteTechnology(){
+		try {
+			TechnologyDAO td = new TechnologyDAO();
+			td.createTechnology("developer1");
+			td.deleteTechnology("developer1");			
+		} catch (Exception ex) {
+			Assert.fail();
+		}
+	}
 }
