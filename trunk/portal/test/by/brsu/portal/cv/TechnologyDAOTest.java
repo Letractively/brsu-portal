d75b34a1e91f77282387369a7d67bd029e03cc45
boie8888@gmail.com <boie8888@gmail.com@ea57eaa0-7ecd-95af-890e-8a5d75c84b2e>
2011-07-31 19:40:46 +0000

    Modified tests.

--- trunk/portal/test/by/brsu/portal/cv/TechnologyDAOTest.java
+++ trunk/portal/test/by/brsu/portal/cv/TechnologyDAOTest.java
@@ -7,6 +7,7 @@ package by.brsu.portal.cv;
 import org.junit.Assert;
 import org.junit.Test;
 
+import by.brsu.portal.PortalTechnicalException;
 import by.brsu.portal.cv.TechnologyDAO;
 
 /**
@@ -15,14 +16,14 @@ import by.brsu.portal.cv.TechnologyDAO;
  */
 public class TechnologyDAOTest {
 	@Test
-	public void testCreateTechnology() {
+	public void testCreateTechnology() throws PortalTechnicalException {
 			TechnologyDAO td = new TechnologyDAO();
 			Technology t1 = td.createTechnology(".NET");
 			//System.out.println("test1 "+t1);
 			Assert.assertTrue(t1!=null);
 	}
 	@Test
-	public void testDeleteTechnology(){
+	public void testDeleteTechnology() throws PortalTechnicalException{
 			TechnologyDAO td = new TechnologyDAO();
 			td.deleteTechnology(".NET");
 			Technology t1 = td.findTechnologyByName(".NET");
