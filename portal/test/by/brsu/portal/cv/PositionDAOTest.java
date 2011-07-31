e9dfd97b29e6ad132c149037653d76c85a09be4a
boie8888 <boie8888@gmail.com>
2011-07-31 19:40:46 +0000

    Modified tests.

--- portal/test/by/brsu/portal/cv/PositionDAOTest.java
+++ portal/test/by/brsu/portal/cv/PositionDAOTest.java
@@ -7,24 +7,26 @@ package by.brsu.portal.cv;
 import org.junit.Assert;
 import org.junit.Test;
 
+import by.brsu.portal.PortalTechnicalException;
+
 /**
  * @author Roman Ulezlo
  * 
  */
 public class PositionDAOTest {
 	@Test
-	public void testCreatePosition() {
+	public void testCreatePosition() throws PortalTechnicalException {
 		PositionDAO td = new PositionDAO();
-		Position t1 = td.createPosition("developer");
+		Position t1 = td.createPosition("developerrr");
 		//System.out.println("test1 "+t1);
 		Assert.assertTrue(t1 != null);
 	}
 
 	@Test
-	public void testDeletePosition() {
+	public void testDeletePosition() throws PortalTechnicalException {
 		PositionDAO td = new PositionDAO();
-		td.deletePosition("developer");
-		Position t1 = td.findPositionByName("developer");
+		td.deletePosition("developerrr");
+		Position t1 = td.findPositionByName("developerrr");
 		//System.out.println("test2 "+t1);
 		Assert.assertTrue(t1 == null);
 	}
