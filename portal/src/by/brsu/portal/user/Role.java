65f5d87b7354fc74730f8edaa05b92bc878d4d91
boie8888 <boie8888@gmail.com>
2011-08-01 09:08:51 +0000

    Add new enum.

---
+++ portal/src/by/brsu/portal/user/Role.java
@@ -0,0 +1,13 @@
+/**
+ * BrSU Projects Portal
+ * (c) 2011, BrSU Java Group
+ */
+package by.brsu.portal.user;
+
+/**
+ * @author Roman Ulezlo
+ * 
+ */
+public enum Role {
+	Admin, User, Moderator;
+}
