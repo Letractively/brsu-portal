980d96abc5440bbfa461edd04eefb83831114904
boie8888 <boie8888@gmail.com>
2011-08-24 14:01:25 +0000

    Add new class.

---
+++ portal/src/by/brsu/portal/servlets/AddProject.java
@@ -0,0 +1,31 @@
+/**
+ * BrSU Projects Portal
+ * (c) 2011, BrSU Java Group
+ */
+package by.brsu.portal.servlets;
+
+/**
+ * @author Admin
+ *
+ */
+public class AddProject implements Action{
+
+	@Override
+	public String perform() {
+		// TODO Auto-generated method stub
+		return "/addProjectOK.jsp";
+	}
+
+	@Override
+	public String nameReq() {
+		// TODO Auto-generated method stub
+		return "";
+	}
+
+	@Override
+	public Object send() {
+		// TODO Auto-generated method stub
+		return "";
+	}
+
+}
