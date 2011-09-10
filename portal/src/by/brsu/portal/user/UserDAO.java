73471ae80acd64da2bee3c954ff2844bddc1e1b9
wertyBDE <wertyBDE@gmail.com>
2011-09-10 14:51:11 +0000

    add function(findUserByEmail) in class UserDAO this is done to verify the existence of a user

--- portal/src/by/brsu/portal/user/UserDAO.java
+++ portal/src/by/brsu/portal/user/UserDAO.java
@@ -162,4 +162,22 @@ public class UserDAO {
 		return null;
 	}
 
+	public boolean findUserByEmail(String email) {
+		Connection conn = ConnectionManager.getConnectorPool().getConnection();
+		String query = "Select * from user where email=?";
+		ResultSet rs = null;
+		PreparedStatement st = null;
+		try {
+			st.setString(1, email);
+			rs = st.executeQuery(query);
+			if (rs.next()) {
+				return true;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+		}
+		return false;
+	}
+
 }
