f95aec9d59c81ff9f4eab97da078497f5ee7bfd2
pm07pasha <pm07pasha@gmail.com>
2011-09-15 13:43:54 +0000

    Show users

---
+++ portal/src/by/brsu/portal/servlets/ShowUserProf.java
@@ -0,0 +1,33 @@
+/**
+ * BrSU Projects Portal
+ * (c) 2011, BrSU Java Group
+ */
+
+
+package by.brsu.portal.servlets;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import by.brsu.portal.user.UserDAO;
+import by.brsu.portal.user.User;
+
+public class ShowUserProf implements Action {
+	private UserDAO DAO = new UserDAO();
+	private List<User> listuser  = DAO.FindAllUser();
+	private Map<String, Object> ma_p = new HashMap<String, Object>();
+
+	
+
+	@Override
+	public Map<String, Object> getParametersMap(HttpServletRequest request,
+			HttpServletResponse response) {
+		ma_p.put("users", listuser);
+		return ma_p;
+	}
+
+}
