e15d25dd37ad6c519223fa99cf3f3f0e316c3916
origami909 <origami909@gmail.com>
2011-09-18 21:13:03 +0000

    modified file, delete some string with errors.

--- portal/WebContent/addProject.jsp
+++ portal/WebContent/addProject.jsp
@@ -13,12 +13,14 @@
 			<tr><td width=170><label for="name">Project name:</label></td> <td width=120><input type="text" name="name" width=100 /></td></tr>
 			<tr><td width=170><label for="owner">Owner name:</label></td> <td width=120><input type="text" name="owner" /></td></tr>
 			<tr><td width=170><label for="description">Description:</label></td> <td width=120><input type="text"	name="description" /></td></tr>
+			<tr><td width=170><label for="datecr">Date Of Creation:</label></td> <td width=120><input type="text"	name="datecr" /></td></tr>
+			<tr><td width=170><label for="datecl">Date Of Closing:</label></td> <td width=120><input type="text"	name="datecl" /></td></tr>
 			<tr><td width=170><label for="category">Category:</label></td> <td width=120><input type="text" name="category" /></td></tr>
 			<tr><td width=170><label for="version">Version:</label></td> <td width=120><input	type="text" name="version" /></td></tr>
 			<tr><td width=170><label for="license">License:</label></td> <td width=120><input	type="text" name="license" /></td></tr>
 			<tr><td width=170><label for="stageOfDevelopment">Stage of development:</label></td> <td width=120><input	type="text" name="stageOfDevelopment" /></td></tr> 
-			<tr><td width=170><label for="technology">Technology:</label></td> <td width=120><input type="text" name="technology" /></td></tr>
-			<tr><td width=170><label for="languages">Languages:</label></td> <td width=120><input	type="text" name="languages" /></td></tr>
+			<!-- <tr><td width=170><label for="technology">Technology:</label></td> <td width=120><input type="text" name="technology" /></td></tr>
+			<tr><td width=170><label for="languages">Languages:</label></td> <td width=120><input	type="text" name="languages" /></td></tr>  -->
 			</table> 
 			<input type="submit" value="Add Project" />
 		</form>
--- portal/src/by/brsu/portal/servlets/AddProject.java
+++ portal/src/by/brsu/portal/servlets/AddProject.java
@@ -4,15 +4,17 @@
  */
 package by.brsu.portal.servlets;
 
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import by.brsu.portal.cv.ProgrammingLanguage;
-import by.brsu.portal.cv.Technology;
+
 import by.brsu.portal.project.Project;
 import by.brsu.portal.project.ProjectCategory;
 import by.brsu.portal.project.ProjectDAO;
@@ -24,28 +26,35 @@ import by.brsu.portal.project.ProjectDAO;
 public class AddProject implements Action {
 	private Map<String, Object> map = new HashMap<String, Object>();
 	Project project = new Project();
-
+	
 	@Override
 	public boolean perform(HttpServletRequest request,
 			HttpServletResponse response) {
-		ProjectCategory pc = new ProjectCategory();
 		project.setName(request.getParameter("name"));
+		project.setUser(Integer.parseInt(request.getParameter("owner")));
 		project.setDescription(request.getParameter("description"));
+		/*DateFormat dateFormat = DateFormat.getDateInstance();
+		Date date = null;
+		try {
+			date = dateFormat.parse(request.getParameter("datecr"));
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		project.setDateOfCreation(date);
+		try {
+			date = dateFormat.parse(request.getParameter("datecl"));
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		project.setDateOfClosing(date);*/
+		ProjectCategory pc = new ProjectCategory();
 		pc.setName(request.getParameter("category"));
 		project.setCategory(pc);
+		project.setVersion(Integer.parseInt(request.getParameter("version")));
 		project.setLicense(request.getParameter("license"));
-		project.setStageOfDevelopment(request
-				.getParameter("stageOfDevelopment"));
-		Technology tech = new Technology();
-		tech.setName(request.getParameter("technology"));
-		List<Technology> th = new ArrayList<Technology>();
-		th.add(tech);
-		project.setTechnology(th);
-		ProgrammingLanguage progl = new ProgrammingLanguage();
-		progl.setName(request.getParameter("languages"));
-		List<ProgrammingLanguage> pl = new ArrayList<ProgrammingLanguage>();
-		pl.add(progl);
-		project.setLanguages(pl);
+		project.setStageOfDevelopment(request.getParameter("stageOfDevelopment"));
 
 		ProjectDAO ppp = new ProjectDAO();
 		ppp.createProject(project);
@@ -58,4 +67,54 @@ public class AddProject implements Action {
 		map.put("project", project);
 		return map;
 	}
+	
+	/*private Map<String, Object> map = new HashMap<String, Object>();
+	Project project = new Project();
+
+	@Override
+	public Map<String, Object> getParametersMap(HttpServletRequest request,
+			HttpServletResponse response) {		
+		project.setName(request.getParameter("name"));
+		project.setUser(Integer.parseInt(request.getParameter("owner")));
+		project.setDescription(request.getParameter("description"));*/
+		/*DateFormat dateFormat = DateFormat.getDateInstance();
+		Date date = null;
+		try {
+			date = dateFormat.parse(request.getParameter("datecr"));
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		project.setDateOfCreation(date);
+		try {
+			date = dateFormat.parse(request.getParameter("datecl"));
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		project.setDateOfClosing(date);*/
+		/*ProjectCategory pc = new ProjectCategory();
+		pc.setName(request.getParameter("category"));
+		project.setCategory(pc);
+		project.setVersion(Integer.parseInt(request.getParameter("version")));
+		project.setLicense(request.getParameter("license"));
+		project.setStageOfDevelopment(request.getParameter("stageOfDevelopment"));*/
+		/*Technology tech = new Technology();
+		tech.setName(request.getParameter("technology"));
+		List<Technology> th = new ArrayList<Technology>();
+		th.add(tech);
+		project.setTechnology(th);
+		ProgrammingLanguage progl = new ProgrammingLanguage();
+		progl.setName(request.getParameter("languages"));
+		List<ProgrammingLanguage> pl = new ArrayList<ProgrammingLanguage>();
+		pl.add(progl);
+		project.setLanguages(pl);*/
+
+		/*ProjectDAO ppp = new ProjectDAO();
+		ppp.createProject(project);
+
+		// return "/addProjectOK.jsp";
+		map.put("project", project);
+		return map;
+	}*/
 }
