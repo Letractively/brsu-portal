326a25fcff273e89a8f56f3e3586d6f54f8d49a6
origami909@gmail.com <origami909@gmail.com@ea57eaa0-7ecd-95af-890e-8a5d75c84b2e>
2011-09-28 20:59:25 +0000

    Add class ProgrammingLanguage DAO and add new interfese IProgrammingLanguage

---
+++ trunk/portal/src/by/brsu/portal/cv/ProgrammingLanguageDAO.java
@@ -0,0 +1,55 @@
+/**
+ * BrSU Projects Portal
+ * (c) 2011, BrSU Java Group
+ */
+package by.brsu.portal.cv;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import by.brsu.portal.ConnectionManager;
+import by.brsu.portal.PortalTechnicalException;
+
+/**
+ * @author Artur Smaliuk
+ *
+ */
+public class ProgrammingLanguageDAO implements IProgrammingLanguageDAO{
+	private Connection conn = null;
+	public List<ProgrammingLanguage> findAllLanguages()
+			throws PortalTechnicalException {
+		conn = ConnectionManager.getConnectorPool().getConnection();
+		String sql = "Select * from language";
+		ResultSet rs = null;
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement("");
+			rs = st.executeQuery(sql);
+			List<ProgrammingLanguage> proglang = new ArrayList<ProgrammingLanguage>();
+			ProgrammingLanguage tempproglang = new ProgrammingLanguage();
+			if (rs.next()) {
+				tempproglang.setIdLanguage(rs.getLong(1));
+				tempproglang.setName(rs.getString(2));
+				proglang.add(tempproglang);
+			}
+			return proglang;
+		} catch (SQLException e) {
+			throw new PortalTechnicalException(
+					"Error of performance of inquiry!");
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (st != null)
+					st.close();
+			} catch (SQLException ex) {
+				throw new PortalTechnicalException(
+						"Error closing object ResultSet or PreparedStatement!");
+			}
+		}
+	}
+}
